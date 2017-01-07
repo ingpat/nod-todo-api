687534af629f04bdceb2cf6ffafd1e6d0634dc16
ingpat <ingpatrickls@gmail.com>
2017-01-07 08:25:55 -0800

    second commit delete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,33 @@
+//var MongoClient = require('mongodb').MongoClient;
+var {MongoClient,ObjectId}=require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+	if(err){
+		return console.log('unable to connect to mongo db server')
+	}
+
+	console.log('Connected to mongo db server')
+
+	// delete many	
+	/*db.collection('Todo').deleteMany({text:'watch my fd'}).then((result)=>{
+		console.log(result);		
+	});*/
+
+
+	//delete one
+	/*db.collection('Todo').deleteOne({text:'watch my fd'}).then((result)=>{
+		console.log(result);		
+	});*/
+
+	//find one and delete
+	db.collection('Todo').findOneAndDelete({completed:true}).then((result)=>{
+		console.log(result);		
+	});
+
+	db.collection('Todo').findOneAndDelete({completed:true}).then((result)=>{
+		console.log(JSON.stringify(result,undefined,2));		
+	});
+
+	db.close();
+}); 
+ 
